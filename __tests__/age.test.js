3dbf78270a88917f138c3b108c5608a3bcca096f
Tawnee Harris <tawneeh@icloud.com>
2020-11-13 09:26:42 -0800

    add test one good fail

---
+++ __tests__/age.test.js
@@ -0,0 +1,20 @@
+import GalacticAge from './../src/age.js';
+
+describe('GalacticAge', () => {
+
+  let galacticAge;
+
+  beforeEach(() => {
+    galacticAge = new GalacticAge("31", "132", "51", "16", "2", "41") // 41 more years
+  });
+
+  test('should correctly create a GalacticAge object', () => {
+    expect(galacticAge.earthAge).toEqual("31");
+    expect(galacticAge.mercuryAge).toEqual("13");
+    expect(galacticAge.venusAge).toEqual("51");
+    expect(galacticAge.marsAge).toEqual("16");
+    expect(galacticAge.jupiterAge).toEqual("2");
+    expect(galacticAge.earthExpectancy).toEqual("41");
+  });
+
+});
---
+++ src/age.js
@@ -0,0 +1,10 @@
+export default class GalacticAge {
+  constructor(earthAge, mercuryAge, venusAge, marsAge, jupiterAge, earthExpectancy) {
+    this.earthAge = earthAge;
+    this.mercuryAge = mercuryAge;
+    this.venusAge = venusAge;
+    this.marsAge = marsAge;
+    this.jupiterAge = jupiterAge; 
+    this.earthExpectancy = earthExpectancy;
+  }
+}
